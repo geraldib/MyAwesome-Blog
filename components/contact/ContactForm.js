b0ce834224254b0247ed524bd9a55226d94a9eae
Gerald Ibra <gi@horologists.com>
2023-10-27 12:47:59 +0200

    Added contact api for adding contacts to db

--- components/contact/ContactForm.js
+++ components/contact/ContactForm.js
@@ -1,27 +1,112 @@
+import { useRef, useState, useEffect } from "react";
+import Notification from "../ui/notification";
 import classes from "./contact-form.module.css";
 const ContactForm = () => {
+  const emailRef = useRef();
+  const nameRef = useRef();
+  const messageRef = useRef();
+  const [requestStatus, setRequestStatus] = useState();
+
+  useEffect(() => {
+    if (requestStatus === "success" || requestStatus === "error") {
+      const timer = setTimeout(() => {
+        setRequestStatus(null);
+      }, 3000);
+      return () => clearTimeout(timer);
+    }
+  }, [requestStatus]);
+
+  const sendMessageHandler = async (e) => {
+    e.preventDefault();
+
+    setRequestStatus("pending");
+
+    const enteredEmail = emailRef.current.value;
+    const enteredName = nameRef.current.value;
+    const enteredMessage = messageRef.current.value;
+
+    let response;
+    try {
+      response = await fetch("/api/contact", {
+        method: "POST",
+        body: JSON.stringify({
+          email: enteredEmail,
+          name: enteredName,
+          message: enteredMessage,
+        }),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+      setRequestStatus("success");
+      emailRef.current.value = "";
+      nameRef.current.value = "";
+      messageRef.current.value = "";
+    } catch (error) {
+      setRequestStatus("error");
+    }
+
+    const data = response.json();
+    if (!response.ok) {
+      throw new Error(data.message || "Something went wrong");
+    }
+  };
+
+  let notification;
+
+  if (requestStatus === "pending") {
+    notification = {
+      status: "pending",
+      title: "Sending message...",
+      message: "Your message is on its way",
+    };
+  }
+
+  if (requestStatus === "success") {
+    notification = {
+      status: "success",
+      title: "Success!",
+      message: "Message sent successfully!",
+    };
+  }
+
+  if (requestStatus === "error") {
+    notification = {
+      status: "error",
+      title: "Error!",
+      message: "Error not sent!",
+    };
+  }
+
   return (
     <section className={classes.contact}>
       <h1>How can I help you?</h1>
-      <form className={classes.form}>
+      <form className={classes.form} onSubmit={sendMessageHandler}>
         <div className={classes.controls}>
           <div className={classes.control}>
             <label htmlFor="email">Your Email</label>
-            <input type="email" id="email" required />
+            <input type="email" id="email" ref={emailRef} required />
           </div>
           <div className={classes.control}>
             <label htmlFor="name">Your Name</label>
-            <input type="text" id="name" required />
+            <input type="text" id="name" ref={nameRef} required />
           </div>
         </div>
         <div className={classes.control}>
           <label htmlFor="message">Your Message</label>
-          <textarea id="message" rows="5"></textarea>
+          <textarea id="message" rows="5" ref={messageRef}></textarea>
         </div>
         <div className={classes.actions}>
           <button>Send Message</button>
         </div>
       </form>
+      {notification && (
+        <Notification
+          title={notification.title}
+          message={notification.message}
+          status={notification.status}
+        />
+      )}
     </section>
   );
 };
