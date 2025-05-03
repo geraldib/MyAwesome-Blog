899dd760a101f1007bd69a4b7906ffea9a8c39cb
Mateo-MmM <merjamateo12@gmail.com>
2025-05-03 15:07:47 +0200

    ChangeName

--- components/homepage/hero.js
+++ components/homepage/hero.js
@@ -6,13 +6,13 @@ const Hero = () => {
     <section className={classes.hero}>
       <div className={classes.image}>
         <Image
-          src="/images/site/me.jpeg"
+          src="/images/site/mateo.png"
           alt="An image of me"
           width={300}
           height={500}
         />
       </div>
-      <h1>Hi, I'm Gerald</h1>
+      <h1>Hi, I'm Mateo</h1>
       <p>
         I blog about web development - mainly platforms using technologies like
         React or PHP/Laravel
--- components/layout/logo.js
+++ components/layout/logo.js
@@ -1,7 +1,7 @@
 import classes from "./logo.module.css";
 
 const Logo = () => {
-  return <div className={classes.logo}>Gerald Blog</div>;
+  return <div className={classes.logo}>Mateo Blog</div>;
 };
 
 export default Logo;
